197eebbf9faecefe339a0402fae2ff3ce8410551
Jpardo107 <usuario@Mac-mini-de-Usuario.local>
2022-09-20 16:15:22 -0300

    request_response_body

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from typing import Optional
+from pydantic import BaseModel 
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+class Person(BaseModel):
+    #Requeridos
+    Nombre: str
+    Apellido: str
+    Edad: int
+    #opcionales
+    colorPelo: Optional[str] = None
+    Casado: Optional[bool] = None
 
 #comando para correr la API
 # uvicorn main:app --reload
@@ -10,3 +22,10 @@ app = FastAPI()
 #path operation functions
 def home():
     return{'Hello':'World'}
+
+#Request and Response Body
+
+@app.post("/person/new")
+
+def createPerson(person:Person = Body(...)):
+    return(person)
